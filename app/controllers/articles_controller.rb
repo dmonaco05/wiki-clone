6d987bf7749fcd5167e1d794ee769ba5a0314ea7
Dominic Monaco <dmonaco05@hotmail.com>
2015-03-05 16:33:20 -0500

    edit & delete articles functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :find_article, only: [:show, :edit, :destroy]
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -25,19 +25,22 @@ class ArticlesController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
-
   end
 
   def update
-
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @article.destory
+    redirect_to root_path
   end
 
   private
