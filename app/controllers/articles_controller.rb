26ced494efa98cd7e27abfa827b8c18f6ed6c9cf
Dominic Monaco <dmonaco05@hotmail.com>
2015-03-05 15:52:38 -0500

    articles mvc & gems

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :destroy]
+
+  def index
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
+    end
+end
